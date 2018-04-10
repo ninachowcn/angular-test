9a20f0b6083939eae5068141a685def3296b2432
Zhang Yuting <ninachow.cn@gmail.com>
2018-04-10 22:37:49 +0800

    hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
+
+
+const HEROES: Hero[] = [
+  { id: 11, name:'MR.nice'},
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`
+    <h1>{{ title }}</h1>  
+    <h2>my heroes</h2>
+    <ul class = "heroes">
+      <li *ngFor = "let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)" >
+        <span class="badge">{{ hero.id }} </span>{{ hero.name }}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+   `,
+   styles:[`
+    .selected{
+      background-color: #cfd8dc !important;
+      color: white;
+    }
+    .heroes{
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li{
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #eee;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #bbd8dc !important;
+      color: white;
+    }
+    .heroes li:hover{
+      color: #607d8b;
+      background-color: #ddd;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      position: relative;
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+   `]
+
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'windstorm'
+  // };
+  selectedHero : Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+// 虽然NgModel是一个有效的Angular指令，但它默认情况下却是不可用的。 它属于一个可选模块FormsModule。 我们必须选择使用那个模块。
+//FormsModule添加到 @NgModule 元数据的imports数组中，它是当前应用正在使用的外部模块列表。
 
 
 import { AppComponent } from './app.component';
+import {HeroDetailComponent} from './hero-detail.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
